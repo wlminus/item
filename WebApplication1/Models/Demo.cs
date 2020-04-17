eec1cb309ab58fdeeacb6236f3e1c684d3a6d64c
HOANGTHANH <thanhhn0902@outlook.com>
2020-04-17 21:05:10 +0700

    Add project files.

---
+++ WebApplication1/Models/Demo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Models
+{
+    [Table("Demo")]
+    public class Demo
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("Name")]
+        [StringLength(500)]
+        public string Name { get; set; }
+        [Column("Age")]
+        public int Age { get; set; }
+    }
+}
